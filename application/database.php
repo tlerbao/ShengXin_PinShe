beee2050512cef7c53beb0aa7f48031ada9a672a
Talent <lnnw@163.com>
2024-08-21 23:24:21 +0800

    Update database parameters for env call

--- application/database.php
+++ application/database.php
@@ -1,4 +1,7 @@
 <?php
+
+use think\Env;
+
 /**
  * 易优CMS
  * ============================================================================
@@ -13,49 +16,49 @@
 
 return array(
     // 数据库类型
-    'type'            => 'mysql',
+    'type'            => Env::get('DB_TYPE', 'mysql'),
     // 服务器地址
-    'hostname'        => 'mysql',
+    'hostname'        => Env::get('DB_HOST', 'mysql'),
     // 数据库名
-    'database'        => 'shengxin_pinshesite',
+    'database'        => Env::get('DB_NAME', 'shengxin_pinshesite'),
     // 用户名
-    'username'        => 'root',
+    'username'        => Env::get('DB_USER', 'root'),
     // 密码
-    'password'        => 'root',
+    'password'        => Env::get('DB_PASSWORD', 'root'),
     // 端口
-    'hostport'        => '3306',
+    'hostport'        => Env::get('DB_PORT', '3306'),
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
     'params'          => array(),
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset'         => Env::get('DB_CHARSET', 'utf8'),
     // 数据库表前缀
-    'prefix'          => 'ta_',
+    'prefix'          => Env::get('DB_PREFIX', 'ta_'),
     // 数据库调试模式
-    'debug'           => false,
+    'debug'           => Env::get('DB_DEBUG', false),
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'deploy'          => 0,
+    'deploy'          => Env::get('DB_DEPLOY', 0),
     // 数据库读写是否分离 主从式有效
-    'rw_separate'     => false,
+    'rw_separate'     => Env::get('DB_RW_SEPARATE', false),
     // 读写分离后 主服务器数量
-    'master_num'      => 1,
+    'master_num'      => Env::get('DB_MASTER_NUM', 1),
     // 指定从服务器序号
-    'slave_no'        => '',
+    'slave_no'        => Env::get('DB_SLAVE_NO', ''),
     // 是否严格检查字段是否存在
-    'fields_strict'   => true,
+    'fields_strict'   => Env::get('DB_FIELDS_STRICT', true),
     // 数据集返回类型
-    'resultset_type'  => 'array',
+    'resultset_type'  => Env::get('DB_RESULTSET_TYPE', 'array'),
     // 自动写入时间戳字段
-    'auto_timestamp'  => false,
+    'auto_timestamp'  => Env::get('DB_AUTO_TIMESTAMP', false),
     // 时间字段取出后的默认时间格式
-    'datetime_format' => 'Y-m-d H:i:s',
+    'datetime_format' => Env::get('DB_DATETIME_FORMAT', 'Y-m-d H:i:s'),
     // 是否需要进行SQL性能分析
-    'sql_explain'     => false,
+    'sql_explain'     => Env::get('DB_SQL_EXPLAIN', false),
     // Builder类
     'builder'         => '',
     // Query类
     'query'           => '\\think\\db\\Query',
     // 是否需要断线重连
-    'break_reconnect' => false,
+    'break_reconnect' => Env::get('DB_BREAK_RECONNECT', false),
 );
